6530eaabd7a1009d740db73329584b5b5ff8c4ee
AdrianaRoza <adriana.roza.aka@gmail.com>
2025-06-13 10:45:04 -0400

    create footer

--- src/App.jsx
+++ src/App.jsx
@@ -6,21 +6,34 @@ import Lunch from './pages/Lunch'
 import Snacks from './pages/Snacks'
 import Drinks from './pages/Drinks'
 import Broths from './pages/Broths'
+import Footer from './components/Footer'
 
 function App() {
 
   return (
+  <>
    <Router>
-    <Routes>
-      <Route path="/" element={ <Home/> }/>
-      <Route path="lunch" element={ <Lunch/> }/>
-      <Route path="dinner" element={ <Dinner/> }/>
-      <Route path="breakfast" element={ <Breakfast/> }/>
-      <Route path="snacks" element={ <Snacks/> }/>
-      <Route path="drinks" element={ <Drinks/> }/>
-      <Route path="broths" element={ <Broths/> }/>
-    </Routes>
+      <div className="min-h-screen flex flex-col">
+      <nav 
+          className="bg-orange-100p-4 flex gap-4 
+            text-orange-800 font-semibold shadow">
+          <Link to="/" className='hover:underline'>Home</Link>
+        </nav>
+        <main className="flex-grow p-4">
+        <Routes>
+          <Route path="/" element={ <Home/> }/>
+          <Route path="lunch" element={ <Lunch/> }/>
+          <Route path="dinner" element={ <Dinner/> }/>
+          <Route path="breakfast" element={ <Breakfast/> }/>
+          <Route path="snacks" element={ <Snacks/> }/>
+          <Route path="drinks" element={ <Drinks/> }/>
+          <Route path="broths" element={ <Broths/> }/>
+        </Routes>
+      </main>
+      </div>
    </Router>
+   <Footer/>
+  </>
   )
 }
 
---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+
+const Footer = () => {
+  return (
+    <footer className="bg-orange-200 text-orange-900 text-center py-4 mt-10">
+      <p>
+      © {new Date().getFullYear()} Adriana Roza. Todos os direitos reservados.
+      </p>
+    </footer>
+  )
+}
+
+export default Footer
