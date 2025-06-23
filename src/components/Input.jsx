bb32f5913970da1654ab3f44cecf24cc6fafffec
AdrianaRoza <adriana.roza.aka@gmail.com>
2025-06-23 08:35:11 -0400

    deleted  category selection

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -34,25 +34,9 @@ const Input = () => {
             focus:outline-none focus:border-orange-500 focus-ring-2 
             focus:ring-orange-800"
         />
-
-        <select
-          value={category}
-          onChange={(e) => setCategory(e.target.value)}
-          className="p-2 text-orange-800 bg-white rounded-md 
-            border-orange-300 mb-2 focus:outline-none 
-            focus:border-orange-500 focus:ring-2 focus:ring-orange-300"
-        >
-          <option value="lanch">Almoço</option>
-          <option value="dinner">Jantar</option>
-          <option value="breakfast">Café da Manhã</option>
-          <option value="snacks">Lanches</option>
-          <option value="drinks">Bebidas</option>
-          <option value="broths">Caldos</option>
-        </select>
-
         <button
           onClick={addRecipe}
-          className='block w-full bg-orange-300 text-white font-bold py-2 
+          className=' bg-orange-300 text-white font-bold py-2 
             px-4 rounded hover:bg-orange-200'
         >
           Adicionar Receita
